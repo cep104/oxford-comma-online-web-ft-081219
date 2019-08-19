0b918ac086e75520c7a5e895f53c8b0515c1fc47
Candice Peters <candicelizabethpeters@gmail.com>
2019-08-19 02:30:37 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -4,7 +4,7 @@ if array.length == 1
 elsif array.length == 2 
   array.join(" and ")
 elsif array.length == 3
-array[0,1].join(', ') + ", and " + array[2]
+array[0..1].join(', ') + ", and " + array[-1]
 elsif array.length > 3
 array[0..-2].join(', ') + ", and " + array[-1]
   
