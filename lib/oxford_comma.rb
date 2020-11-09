e766767afe31e7ccdd24db3278d2bd4f14fd1b25
cep104 <candicelizabethpeters@gmail.com>
2020-11-09 00:12:58 -0600

    add comments

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,14 @@
 def oxford_comma(array)
+    if array.length == 2 
+    #if the array equals to join with and
+    return array.join(" and ")
 
+    
+    elsif 2 < array.length 
+        array[-1].insert(0, "and ")
+        #on the last array element at the start insert "and " 
+        #example if you put (1, "and ") it would return sand tarfruit insead of "and starfruit"
+    end
+    array.join(", ") #turn the array into a string seperated by a comma
+   
 end
